4a97f95e0888ba41ceb178f7b098e02f269c30ca
chisoftltd <chisoftltd@gmail.com>
2021-03-15 17:34:10 +0100

    Python MySQL

--- PythonMySQL.py
+++ PythonMySQL.py
@@ -1,8 +1,28 @@
 # Python MySQL
-import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost"
-)
+import pyodbc
+conn_str = (r'Driver={SQL Server};'
+            r'Server=DESKTOP-CHINWEU;'
+            r'Database=Customer;'
+            r'Trusted_Connection=yes;')
+# cnxn = pyodbc.connect('Driver=SQL Server;Server= .\DESKTOP-CHINWEU;Database=Customer;Trusted_Connection=yes;')
+cnxn = pyodbc.connect(conn_str)
+cursor = cnxn.cursor()
+cursor.execute('SELECT name FROM master.dbo.sysdatabases')
+for X in cursor:
+  print(X)
+print()
 
-print(mydb)
+conn_str = (r'Driver={SQL Server};'
+            r'Server=DESKTOP-CHINWEU;'
+            r'Database=Customer;'
+            r'Trusted_Connection=yes;')
+# cnxn = pyodbc.connect('Driver=SQL Server;Server= .\DESKTOP-CHINWEU;Database=Customer;Trusted_Connection=yes;')
+cnxn = pyodbc.connect(conn_str)
+cursor = cnxn.cursor()
+cursor.execute('SELECT * FROM Customer')
+print(cursor)
+for row in cursor:
+  print(row)
+print()
+cnxn.close()
